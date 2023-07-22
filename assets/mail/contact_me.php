dc99a45d09df51f9ca561ee0ab7f44e0f5d0694b
GPCDOMINIAN <140275466+GPCDOMINIAN@users.noreply.github.com>
2023-07-22 14:55:20 -0500

    Update contact_me.php

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -9,7 +9,7 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "yourname@yourdomain.com";
+$to = "sayyedg@hotmail.com";
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@yourdomain.com\n";
